837d028937ab195edf165bdb234fb201fc2a580b
Lenzinger <luke.lenzinger@gmail.com>
2025-02-05 14:16:19 -0500

    Added Weather Function - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,15 @@
-# Programmer: Luke Lenzinger
-# Branch: Weather
-# Date:
-# Program:
+print("\n*********************************\n")
+
+print("Weather Branch - Developer: Luke Lenzinger\n")
+
+#Import Libraries Here!
+import random
+from time import sleep
+
+#Weather Function to determine weather
+def weather():
+    weatherForecastList = ["snowing", "blizzard", "icy", "rain", "windy", "sunny",]
+    weatherCondition = random.choice(weatherForecastList)
+    return weatherCondition
+
+print(weather())
