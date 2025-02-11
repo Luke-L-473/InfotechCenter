619cdfe288ec5d99c10dd4ab037b98281c4620fc
Lenzinger <luke.lenzinger@gmail.com>
2025-02-11 13:45:45 -0500

    Added Sleep and engaged VRS Stystem for Speed

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,6 +1,6 @@
 print("\n*********************************\n")
 
-print("Weather Branch - Developer: Luke Lenzinger\n")
+print("Weather Branch - Developer: Luke Lenzinger")
 
 #Import Libraries Here!
 import random
@@ -18,19 +18,31 @@ def vehicleResponseSystem():
     if weatherAlert == "snowy":
         print("\nThe National Weather Service has updated your alarm by 30 minutes because"
               " of the forecasted", weatherAlert, "weather condiditons.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 45MPH")
     elif weatherAlert == "blizzard":
         print("\nThe National Weather Service has updated your alarm by 60 minutes because"
               " of the forecasted", weatherAlert, "conditions.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 35MPH")
     elif weatherAlert == "icy":
         print("\nThe National Weather Service has updated your alarm by 60 minutes because"
               " of the forecasted", weatherAlert, "road conditions.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 45MPH")
     elif weatherAlert == "rainy":
         print("\nThe National Weather Service has updated your alarm by 15 minutes because"
               " of the forecasted", weatherAlert, "weather.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 55MPH")
     elif weatherAlert == "windy":
         print("\nThe National Weather Service has updated your alarm by 10 minutes because"
               " of the forecasted", weatherAlert, "conditions.")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 60MPH")
     else:
         print("\nThe National Weather Service is calling for", weatherAlert, "skys outside, drive safe!")
+        sleep(1)
+        print("VRS has been engaged only allowing us to drive 70MPH")
         
 vehicleResponseSystem()
