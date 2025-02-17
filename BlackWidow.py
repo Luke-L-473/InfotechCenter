52470f990fa7793ed0f9f8bbaea90eb9a286c6d5
Luke-L-473 <luke.lenzinger@gmail.com>
2025-02-17 14:08:01 -0500

    Changed Spacing and Tested - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -19,20 +19,20 @@ def gasLevelAlert():
     if gasLevelIndicator == "Empty":
         print("****WARNING - YOU ARE OUT OF GAS****\n")
         sleep(1.25)
-        print("Calling AAA")
+        print("Calling AAA\n")
     elif gasLevelIndicator == "Low":
-        print("Your gas tank is extremely low, checking GPS for the closest gas station")
+        print("Your gas tank is extremely low, checking GPS for the closest gas station.\n")
         sleep(1.25)
-        print("The cosest gas station is", gasStations(), "which is", milesToGasStationLow, "miles away.")
+        print("The cosest gas station is", gasStations(), "which is", milesToGasStationLow, "miles away.\n")
     elif gasLevelIndicator == "Quarter Tank":
-        print("Your gas tank is a Quarter Tank full, checking GPS for the closest gas station")
+        print("Your gas tank is a Quarter Tank full, checking GPS for the closest gas station.\n")
         sleep(1.25)
-        print("The cosest gas station is", gasStations(), "which is", milesToGasStationQuarterTank, "miles away.")
+        print("The cosest gas station is", gasStations(), "which is", milesToGasStationQuarterTank, "miles away.\n")
     elif gasLevelIndicator == "Half Tank":
-        print("Your gas tank is on a Half of a Tank, which is plenty to get to your destination!")
+        print("Your gas tank is on a Half of a Tank, which is plenty to get to your destination!\n")
     elif gasLevelIndicator == "Three Quarter Tank":
-        print("Your gas tank is Three Quarters of a Tank full, which is more than a enough to get to your destination!")
+        print("Your gas tank is Three Quarters of a Tank full, which is more than a enough to get to your destination!\n")
     else:
-        print("You have a Full Tank of gas, which is more than enough to get you to your destination!")
+        print("You have a Full Tank of gas, which is more than enough to get you to your destination!\n")
 
 gasLevelAlert()        
